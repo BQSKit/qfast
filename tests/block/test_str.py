61bb9ac3bca27c5c23a592f16bec8de78d60cb33
Ed Younis <edyounis123@gmail.com>
2020-03-19 15:05:43 -0700

    Added more block tests

---
+++ tests/block/test_str.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy      as np
+
+from qfast import Block
+
+
+class TestBlockStr ( tf.test.TestCase ):
+
+    TOFFOLI = np.asarray(
+            [[1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j, 0.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j],
+            [0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 0.+0.j, 1.+0.j, 0.+0.j]] )
+
+    def test_block_str_1 ( self ):
+        block = Block( self.TOFFOLI, (0, 1, 2) )
+        self.assertEqual( str( block ),
+                          str( (0, 1, 2) ) + ":" + str( self.TOFFOLI ) )
+
+    def test_block_str_2 ( self ):
+        block = Block( self.TOFFOLI, (4, 5, 7) )
+        self.assertEqual( str( block ),
+                          str( (4, 5, 7) ) + ":" + str( self.TOFFOLI ) )
