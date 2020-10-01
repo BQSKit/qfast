3cb3beff09b41b5ee204a1f98d1a9dc7b6bd52f3
Ed Younis <edyounis123@gmail.com>
2020-10-01 08:16:03 -0700

    Tests

--- tests/utils/test_softmax.py
+++ tests/utils/test_softmax.py
@@ -1,13 +1,26 @@
 import numpy    as np
 import unittest as ut
 
-from qfast.utils import item
+from qfast.utils import softmax
 
 
-class TestItem ( ut.TestCase ):
+class TestSoftmax ( ut.TestCase ):
     
-    def test_item_ ( self ):
-        self.assertTrue( True )
+    def test_softmax1 ( self ):
+        for i in range( 10 ):
+            x = 10 * np.random.random( 100 )
+            self.assertTrue( np.abs( np.sum( softmax( x ) ) - 1 ) < 1e-15 )
+
+    def test_softmax2 ( self ):
+        x = np.ones( 10 )
+        x[0] = 2
+        self.assertTrue( np.argmax( softmax( x ) ) == 0 )
+        x[0] = 1
+        x[5] = 2
+        self.assertTrue( np.argmax( softmax( x ) ) == 5 )
+
+    def test_softmax_invalid ( self ):
+        self.assertRaises( TypeError, softmax, "a" )
     
 
 if __name__ == '__main__':
